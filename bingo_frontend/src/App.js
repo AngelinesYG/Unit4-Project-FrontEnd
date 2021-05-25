202f46e42ca672c37a65c0ac394e46457b184adb
cheisentrout <cheisentrout@gmail.com>
2021-05-25 14:48:07 -0400

    Created React app with buildpack, updated code in app.js

---
+++ bingo_frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is Bingo the App</h1>
+            </div>
+        )
+    }
+}
+
+export default App
