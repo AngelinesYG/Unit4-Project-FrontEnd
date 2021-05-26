73b36d3fa4cc7c3aca42ba86a20ba3bdbf3ca547
cheisentrout <cheisentrout@gmail.com>
2021-05-26 12:31:14 -0400

    Built component file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Nav from './components/Nav'
+import HeroArea from './components/HeroArea'
+import DogProf from './components/DogProf'
+import Weather from './components/Weather'
+import Map from './components/Map'
+import Footer from './components/Footer'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="main-container">
+                <Nav />
+                <HeroArea />
+                <DogProf />
+                <Weather />
+                <Map />
+                <Footer />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
